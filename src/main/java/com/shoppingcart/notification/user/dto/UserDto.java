bf92b3e83057977d13371e5ee6b191eeb2a8abfd
jorge pallol <jorgepallol1@gmail.com>
2024-09-04 20:31:19 +0200

    feature: add fields to userDto

--- src/main/java/com/shoppingcart/notification/user/dto/UserDto.java
+++ src/main/java/com/shoppingcart/notification/user/dto/UserDto.java
@@ -1,4 +1,18 @@
 package com.shoppingcart.notification.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor   // new UserDto()
+@AllArgsConstructor //  new UserDto(email,......)
 public class UserDto {
+    private String email;
+    private String name;
+    private String phoneNumber;
+    private String address;
+    private Date createdAt;
 }
